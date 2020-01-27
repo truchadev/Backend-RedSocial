efd95608b12ca050fb4242ee7adaa59b2b85e0bb
Laura Cruz <lcc4413@gmail.com>
2020-01-27 16:45:15 +0100

    cambios en el modelo empresa

--- database/migrations/2020_01_27_151924_create_empresas_table.php
+++ database/migrations/2020_01_27_151924_create_empresas_table.php
@@ -15,6 +15,11 @@ class CreateEmpresasTable extends Migration
     {
         Schema::create('empresas', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
