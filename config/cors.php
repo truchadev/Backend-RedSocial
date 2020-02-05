6bf91c65eeade9d31e707b47487c0238dfbac4eb
Laura Cruz <lcc4413@gmail.com>
2020-02-05 09:54:44 +0100

    cors spatie

--- config/cors.php
+++ config/cors.php
@@ -3,58 +3,59 @@
 return [
 
     /*
-    |--------------------------------------------------------------------------
-    | Laravel CORS Options
-    |--------------------------------------------------------------------------
-    |
-    | The allowed_methods and allowed_headers options are case-insensitive.
-    |
-    | You don't need to provide both allowed_origins and allowed_origins_patterns.
-    | If one of the strings passed matches, it is considered a valid origin.
-    |
-    | If array('*') is provided to allowed_methods, allowed_origins or allowed_headers
-    | all methods / origins / headers are allowed.
-    |
-    */
-
-    /*
-     * You can enable CORS for 1 or multiple paths.
-     * Example: ['api/*']
+     * A cors profile determines which origins, methods, headers are allowed for
+     * a given requests. The `DefaultProfile` reads its configuration from this
+     * config file.
+     *
+     * You can easily create your own cors profile.
+     * More info: https://github.com/spatie/laravel-cors/#creating-your-own-cors-profile
      */
-    'paths' => ['api/*'],
+    'cors_profile' => Spatie\Cors\CorsProfile\DefaultProfile::class,
 
     /*
-    * Matches the request method. `[*]` allows all methods.
-    */
-    'allowed_methods' => ['*'],
-
-    /*
-     * Matches the request origin. `[*]` allows all origins.
+     * This configuration is used by `DefaultProfile`.
      */
-    'allowed_origins' => ['*'],
+    'default_profile' => [
 
-    /*
-     * Matches the request origin with, similar to `Request::is()`
-     */
-    'allowed_origins_patterns' => [],
+        'allow_credentials' => false,
 
-    /*
-     * Sets the Access-Control-Allow-Headers response header. `[*]` allows all headers.
-     */
-    'allowed_headers' => ['*'],
+        'allow_origins' => [
+            '*',
+        ],
 
-    /*
-     * Sets the Access-Control-Expose-Headers response header.
-     */
-    'exposed_headers' => false,
+        'allow_methods' => [
+            'POST',
+            'GET',
+            'OPTIONS',
+            'PUT',
+            'PATCH',
+            'DELETE',
+        ],
 
-    /*
-     * Sets the Access-Control-Max-Age response header.
-     */
-    'max_age' => false,
+        'allow_headers' => [
+            'Content-Type',
+            'X-Auth-Token',
+            'Origin',
+            'Authorization',
+        ],
 
-    /*
-     * Sets the Access-Control-Allow-Credentials header.
-     */
-    'supports_credentials' => false,
+        'expose_headers' => [
+            'Cache-Control',
+            'Content-Language',
+            'Content-Type',
+            'Expires',
+            'Last-Modified',
+            'Pragma',
+        ],
+
+        'forbidden_response' => [
+            'message' => 'Forbidden (cors).',
+            'status' => 403,
+        ],
+
+        /*
+         * Preflight request will respond with value for the max age header.
+         */
+        'max_age' => 60 * 60 * 24,
+    ],
 ];
